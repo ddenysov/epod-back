097ada6ae2e5ba29ce8ebfaa7288b6372bae1aa7
Dmytro Denysov <dmytro.denysov@danads.se>
2022-08-17 12:46:38 +0300

    Prefetch link works

---
+++ src/Controller/ComingSoonController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Service\Builder\Element;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ComingSoonController extends AbstractController
+{
+    /**
+     *
+     * @return JsonResponse
+     */
+    #[Route('/api/coming-soon', name: 'coming_soon')]
+    public function index(): JsonResponse
+    {
+        $root = new Element('div');
+
+        $body = new Element('ui-body');
+        $wrapper = new Element('ui-wrapper');
+
+
+        $wrapper->appendChild(new Element('app-coming-soon'));
+        $body->appendChild($wrapper);
+
+        $root->appendChild($body);
+
+        return new JsonResponse($root->toArray());
+    }
+}
