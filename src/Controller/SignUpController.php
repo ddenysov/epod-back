879da05cd67df7f421b2a8d005cec8abd6a6cadb
Dmytro Denysov <dmytro.denysov@danads.se>
2022-08-22 17:43:17 +0300

    add signup page

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-
 use App\Entity\User;
-use App\Form\Event\SearchType;
 use App\Form\UserType;
 use App\Service\Builder\Element;
-
 use App\Service\Builder\Form\FormBuilder;
+use App\Service\Builder\TextElement;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -20,12 +21,16 @@ class SignUpController extends AbstractController
      *
      * @return JsonResponse
      */
-    #[Route('/api/sign_up', name: 'coming_soon')]
+    #[Route('/api/sign_up', name: 'sign_up')]
     public function index(): JsonResponse
     {
         $root = new Element('div');
 
-        $body = new Element('ui-body');
+        $body = new Element('ui-body', [
+            'props' => [
+                'name' => 'body',
+            ]
+        ]);
         $container = new Element('ui-container', [
             'props' => [
                 'align' => 'center',
@@ -41,8 +46,8 @@ class SignUpController extends AbstractController
 
 
         $form = $this->createForm(UserType::class, new User(), [
-            'action' => '/events',
-            'method' => 'GET',
+            'action' => '/sign_up/store',
+            'method' => 'POST',
         ]);
 
         $formBuilder = new FormBuilder();
@@ -56,5 +61,46 @@ class SignUpController extends AbstractController
         $root->appendChild($body);
 
         return new JsonResponse($root->toArray());
+    }
+
+    /**
+     * @param Request $request
+     * @param ManagerRegistry $manager
+     * @return JsonResponse
+     */
+    #[Route('/api/sign_up/store', name: 'store_user')]
+    public function store(Request $request, ManagerRegistry $manager): JsonResponse
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($request->toArray());
+        $errors = [];
+        $valid = true;
+
+        foreach ($form->all() as $item) {
+            $errors[$item->getName()] = [];
+
+
+            if(!$item->isValid()) {
+                $valid = false;
+                $errors[$item->getName()][] = $item->getErrors()->current()->getMessage();
+            }
+        }
+
+        if (!$valid) {
+            return new JsonResponse(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $manager->getManager()->persist($user);
+        $manager->getManager()->flush();
+
+        $element = new Element('ui-body', [
+            'props' => [
+                'name' => 'body',
+            ]
+        ], [new Element('ui-dialog')]);
+
+        return new JsonResponse($element->toArray(), Response::HTTP_CREATED);
     }
 }
